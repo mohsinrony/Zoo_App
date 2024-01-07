9d82d794e491e1d584943ee16532533753e89b84
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-07 22:23:49 +0200

    bring changes to about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -5,12 +5,12 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="about-content">
-        <h2>About Us</h2>
+        <h2>About This Project</h2>
         <p>
           This project is a React Zoo App that allows users to explore information about animals, birds, fishes and butterflies.
         </p>
         <div className="about-features">
-          <h3>Features Implemented:</h3>
+          <h2>Features Implemented:</h2>
           <ul>
             
             <li>Home Page with links to Animals, Birds, Fishes and Butterfly pages.</li>
@@ -23,53 +23,50 @@ const About = () => {
           </ul>
         </div>
         <div className='learning'>
+            <h2>Learning Outcomes:</h2>
         <p>
           While working on this project, I have learned...
         </p>
-        <h2>React.js Proficiency:</h2>
+        <h3>React.js Proficiency:</h3>
         <p>"Explored and deepened my understanding of React.js, gaining proficiency in building dynamic and interactive user interfaces."</p>
 
 
-        <h2>Routing and Navigation:</h2>
+        <h3>Routing and Navigation:</h3>
         <p>"Implemented efficient navigation and routing using React Router, allowing seamless transitions between different sections of the application."</p>
 
 
-        <h2>State Management:</h2>
+        <h3>State Management:</h3>
         <p>"Gained hands-on experience in state management within React components, ensuring smooth data flow and updates across the application."</p>
 
-        <h2>Responsive Design:</h2>
+        <h3>Responsive Design:</h3>
 
         <p>"Focused on creating a responsive design to ensure a consistent and visually appealing user experience across various devices and screen sizes."</p>
 
-        <h2>Search Functionality:</h2>
+        <h3>Search Functionality:</h3>
 
         <p>"Implemented search functionality, allowing users to easily find specific animals and birds by name, enhancing the overall user experience."</p>
 
-        <h2>CSS Styling:</h2>
+        <h3>CSS Styling:</h3>
         <p>"Explored and applied modern CSS styling techniques to achieve an aesthetically pleasing and professional look for the application."</p>
 
-        <h2>Project Structure and Organization:</h2>
+        <h3>Project Structure and Organization:</h3>
 
         <p>"Learned to structure the project effectively, organizing components and files for better code maintainability and scalability."</p>
 
-        <h2>Collaboration and Teamwork:</h2>
-
-        <p>"Enhanced collaboration and teamwork skills by working on a project that involved multiple components and aspects, ensuring a cohesive final product."</p>
-
-        <h2>Debugging and Troubleshooting:</h2>
+        <h3>Debugging and Troubleshooting:</h3>
 
         <p>"Developed skills in debugging and troubleshooting, addressing various challenges encountered during the development process."</p>
-        <h2>Documentation Practices:</h2>
+        <h3>Documentation Practices:</h3>
 
         <p>"Emphasized the importance of clear and concise documentation to facilitate understanding and future maintenance of the codebase."</p>
-        <h2>User Interface Design:</h2>
+        <h3>User Interface Design:</h3>
 
         <p>"Explored principles of user interface design, aiming for a user-friendly and intuitive layout that enhances overall usability."</p>
 
-        <h2>Version Control (Git):</h2>
+        <h3>Version Control (Git):</h3>
         <p>"Utilized version control with Git, enabling effective collaboration, code versioning, and the ability to revert to previous states if needed."</p>
 
-        <h2>Continuous Learning:</h2>
+        <h3>Continuous Learning:</h3>
         <p>"Cultivated a mindset of continuous learning, staying updated with best practices and exploring new technologies within the ever-evolving field of web development."</p>
 
       </div>
