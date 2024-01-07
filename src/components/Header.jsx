5b68d92a91191f763ef9776c1c3b9a43d2d3fa04
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-07 07:21:56 +0200

    fix the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
-import {NavLink} from 'react-router-dom';
-import "../styles/Header.css"
+import { NavLink } from 'react-router-dom';
+import "../styles/Header.css";
+
 const Header = () => {
-    return <header>
-    <nav>
+  return (
+    <header>
+      <nav>
         <ul>
-            <li><NavLink to="/home"></NavLink>Home</li> <li><NavLink to="/about"></NavLink>About</li>
-            <li><NavLink to="/animals">Animals</NavLink></li>
-            <li><NavLink to="/birds">Birds</NavLink></li>
-            <li><NavLink to="/fishes">Fishes</NavLink></li>
-            <li><NavLink to="/butterflies">Butterflies</NavLink></li>
-            
+          <li><NavLink to="/" exact>Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/animals">Animals</NavLink></li>
+          <li><NavLink to="/birds">Birds</NavLink></li>
+          <li><NavLink to="/fishes">Fishes</NavLink></li>
+          <li><NavLink to="/butterflies">Butterflies</NavLink></li>
         </ul>
-   </nav>
- </header> 
- }
- export default Header;
+      </nav>
+    </header>
+  );
+};
 
-
+export default Header;
