486645c020c0d1e4f7f2c05f9dd430f16287cfa7
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-07 07:34:36 +0200

    add images to link to other pages from homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,27 @@
-function Home() {
-return (
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
-    <>
-    
-    <p>This is home page</p>
- 
-    </>
-)
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <div className='home-content'>
+      <h2>Welcome to the Zoo</h2>
+      <Link to="/animals">
+        <img src="./src/assets/animals.jpg" alt="Animals Page" />
+      </Link>
+      <Link to="/birds">
+        <img src="./src/assets/birds.jpeg" alt="Birds Page" />
+      </Link>
+      <Link to="/fishes">
+        <img src="./src/assets/fish.jpeg" alt="Fishes Page" />
+      </Link>
+      <Link to="/butterflies">
+        <img src="./src/assets/Butterfly.jpeg" alt="Butterfly Page" />
+      </Link>
+      </div>
+    </div>
+  );
 };
+
 export default Home;
